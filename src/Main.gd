205987add93df948bf8e8334f135e6d92ce961f8
andrew-wilkes <github@urgentclick.com>
2022-05-04 14:18:44 +0900

    Add test buttons

--- src/Main.gd
+++ src/Main.gd
@@ -1,13 +1,28 @@
 extends Control
 
 var settings: Settings
+var passwords: Passwords
 
 func _ready():
 	settings = Settings.new()
 	settings = settings.load_data()
-	#settings.save_data()
-	var passwords = Passwords.new()
-	passwords.test(settings)
-	passwords.save_data(settings)
+	passwords = Passwords.new()
 	var pwd = passwords.load_data(settings)
-	print(pwd)
+	if pwd == null:
+		passwords.set_iv()
+	else:
+		passwords = pwd
+	print("ok")
+
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_ESCAPE:
+			get_tree().quit()
+
+
+# Handle shutdown of App
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
+		settings.save_data()
+		passwords.save_data(settings)
--- src/Test.gd
+++ src/Test.gd
@@ -1,13 +1,15 @@
 extends Control
 
+var settings
+
 func _ready():
-	var settings = Settings.new()
-	test_get_char(settings)
-	test_generate_salt(settings)
-	test_enc_dec(settings)
+	settings = Settings.new()
+	test_get_char()
+	test_generate_salt()
+	test_enc_dec()
 
 
-func test_enc_dec(settings):
+func test_enc_dec():
 	var passwords = Passwords.new()
 	passwords.set_iv()
 	prints("IV:", passwords.iv)
@@ -20,14 +22,33 @@ func test_enc_dec(settings):
 	print("Passed")
 
 
-func test_get_char(settings):
+func test_get_char():
 	var chrs = PoolStringArray()
 	for n in 36:
 		chrs.append(settings.get_char(n))
 	prints("Chars:", chrs)
 
 
-func test_generate_salt(settings):
+func test_generate_salt():
 	for n in 10:
 		prints("Salt:", settings.generate_salt())
 
+
+
+func _on_EraseSettings_pressed():
+	var dir = Directory.new()
+	if dir.remove(settings.FILE_NAME) == OK:
+		$AcceptDialog.dialog_text = "Deleted " + settings.FILE_NAME
+	else:
+		$AcceptDialog.dialog_text = settings.FILE_NAME + " not found."
+	$AcceptDialog.popup_centered()
+
+
+func _on_ErasePWD_pressed():
+	var dir = Directory.new()
+	var fn = "user://" + settings.pw_file
+	if dir.remove(fn) == OK:
+		$AcceptDialog.dialog_text = "Deleted " + fn
+	else:
+		$AcceptDialog.dialog_text = fn + " not found."
+	$AcceptDialog.popup_centered()
