0c85dbcd61774a0ab54d146d22d0a60c3dafbdf6
andrew-wilkes <github@urgentclick.com>
2022-05-10 21:32:48 +0900

    Add note about salt usage

--- src/settings.gd
+++ src/settings.gd
@@ -21,8 +21,9 @@ func load_data():
 
 func generate_salt():
 	# Using a-z0-9 characters so that a user may copy them easily to another device
+	# We will not store the salt value in the password db file, otherwise it defeats its purpose
 	var _salt = PoolStringArray()
-	for n in 8 + randi() % 5: # 8 .. 12
+	for _n in 8 + randi() % 5: # 8 .. 12
 		var x = randi() % 36
 		_salt.append(get_char(x))
 	return _salt.join("")
