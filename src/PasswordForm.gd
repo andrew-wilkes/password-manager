94676f86ffc3127674caf91c8fbb41ba479e1243
andrew-wilkes <github@urgentclick.com>
2022-05-28 15:47:17 +0900

    Add password form

---
+++ src/PasswordForm.gd
@@ -0,0 +1,28 @@
+extends VBoxContainer
+
+signal enter_pressed
+signal password_text_changed(txt)
+signal browse_pressed
+
+func _on_Enter_pressed():
+	emit_signal("enter_pressed")
+
+
+func _on_Hidden_pressed():
+	$HBox/Hidden.hide()
+	$HBox/Visible.show()
+	$HBox/Password.secret = false
+
+
+func _on_Visible_pressed():
+	$HBox/Hidden.show()
+	$HBox/Visible.hide()
+	$HBox/Password.secret = true
+
+
+func _on_Password_text_changed(new_text):
+	emit_signal("password_text_changed", new_text)
+
+
+func _on_Browse_pressed():
+	emit_signal("browse_pressed")
