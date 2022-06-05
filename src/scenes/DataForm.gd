426d7cefbfd82e6e1debff2fd5e5d554354307a5
andrew-wilkes <github@urgentclick.com>
2022-06-05 17:28:13 +0900

    Add details view button

--- src/scenes/DataForm.gd
+++ src/scenes/DataForm.gd
@@ -11,6 +11,7 @@ export(Color) var highlight_color
 var heading_scene = preload("res://scenes/Heading.tscn")
 var cell_scene = preload("res://scenes/Cell.tscn")
 var group_button = preload("res://scenes/GroupButton.tscn")
+var view_button = preload("res://scenes/ViewButton.tscn")
 var grid: GridContainer
 var headings = {
 	"title": "Title",
@@ -35,13 +36,31 @@ func populate_grid(db: Database, key, reverse, group):
 		db.order_items(key, reverse)
 	for idx in range(grid.columns, grid.get_child_count()):
 		grid.get_child(idx).queue_free()
+	var idx = grid.columns
 	for item in db.items:
 		if group > 0 and item.group != group:
 			continue
+		var vb = view_button.instance()
+		var _e = vb.connect("view_button_pressed", self, "show_item_details")
+		grid.add_child(vb)
+		vb.idx = idx
+		idx += 1
 		for key in headings:
 			var cell = cell_scene.instance()
 			cell.set_text(get_cell_content(item, key), key == "url")
 			grid.add_child(cell)
+			idx += 1
+
+
+func show_item_details(cell_idx, show):
+	if show:
+		for idx in range(grid.columns, grid.get_child_count(), grid.columns):
+			if idx != cell_idx:
+				grid.get_child(idx).reset()
+		# Show details
+	else:
+		# Hide details
+		pass
 
 
 func add_bars():
