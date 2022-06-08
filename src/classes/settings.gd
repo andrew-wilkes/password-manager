c956dadbc171b339d6daf371fd9588aba45fdbeb
andrew-wilkes <github@urgentclick.com>
2022-06-08 18:59:56 +0900

    Add long salt generation option

--- src/classes/settings.gd
+++ src/classes/settings.gd
@@ -24,14 +24,22 @@ func load_data():
 		return self
 
 
-func generate_salt():
-	# Using a-z0-9 characters so that a user may copy them easily to another device
-	# We will not store the salt value in the password db file, otherwise it defeats its purpose
-	var _salt = PoolStringArray()
-	for _n in 8 + randi() % 5: # 8 .. 12
-		var x = randi() % 36
-		_salt.append(get_char(x))
-	return _salt.join("")
+func generate_salt(short = true):
+	# In the UI it is called a Key
+	if short:
+		# Using a-z0-9 characters so that a user may copy them easily to another device
+		# We will not store the salt value in the password db file, otherwise it defeats its purpose
+		var _salt = PoolStringArray()
+		for _n in 8 + randi() % 5: # 8 .. 12
+			var x = randi() % 36
+			_salt.append(get_char(x))
+		return _salt.join("")
+	else:
+		# Convert a random number to a hash string and then trim the hash so
+		# that it is not the typical length of a hash
+		var h = str(randi()).sha256_text()
+		var i = randi() % 8 + 1
+		return h.substr(i)
 
 
 func get_char(x):
--- src/test/test_settings.gd
+++ src/test/test_settings.gd
@@ -9,9 +9,14 @@ func test_get_char():
 	assert_eq(settings.get_char(35), "z")
 
 func test_generate_salt():
+	# Short user-friendly
 	for n in 10:
 		var salt = settings.generate_salt()
 		if n == 0:
 			assert_typeof(salt, TYPE_STRING)
 		assert_true(salt.length() < 13, "Length <= 12")
 		assert_true(salt.length() > 7, "Length >= 8")
+	# Long computer only
+	var salt = settings.generate_salt(false)
+	assert_true(salt.length() > 20, "Length > 20")
+	assert_ne(salt, "long salt")
